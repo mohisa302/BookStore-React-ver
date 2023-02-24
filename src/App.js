86b4e88e7033303cf3d02f5451e58dc152c8471b
mohi <mohisadeghi302@gmail.com>
2023-02-24 15:44:51 +0330

    Removed bookstoreapp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './styles/App.css';
-import BookStoreApp from './components/bookStoreApp';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Navbar from './components/Navbar';
+import NotMatch from './components/NotMatch';
+import styles from './styles/bookStoreApp.module.css';
 
 const App = () => (
-  <>
-    <BookStoreApp />
-  </>
+  <div className={styles.pageContainer}>
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="/Books" element={<Books />} />
+        <Route path="/" element={<Navigate to="/Books" />} />
+        <Route path="/Categories" element={<Categories />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
+  </div>
 );
 export default App;
